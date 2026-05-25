c2d365b19d0b10dc3b707d6367a85549736b1460
Деревянко Іван <ivan.derevianko@nure.ua>
2026-05-25 16:05:13 +0300

    Initial commit: додано README.md та .gitignore

---
+++ readme.md
@@ -0,0 +1,25 @@
+# README.md
+
+# BSE LR1
+
+Лабораторна робота №1 з Git та GitHub.
+
+## Технології
+
+- HTML
+- CSS
+- JavaScript
+- Git
+- GitHub
+- GitHub Copilot
+
+## Функціональність
+
+- Додавання завдань
+- Робота з Git та GitHub
+- Pull Request
+- AI-генерація коду
+
+## Автор
+
+Іван Деревянко
---
+++ script.js
@@ -0,0 +1,27 @@
+// script.js
+
+function addTask() {
+
+    const input = document.getElementById("taskInput");
+    const taskText = input.value;
+
+    if (taskText === "") {
+        alert("Введіть завдання");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
+
+
+// AI generated function
+function calculateTasks(tasksCount) {
+    return tasksCount * 2;
+}
+
+console.log(calculateTasks(5));
